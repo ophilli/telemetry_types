ceb991730d70b8218f2d547ac031c1e10ba45078
Christian Heussy <heussy@gmail.com>
2024-12-15 11:31:00 -0800

    feat: initial commit
    Problem: Need to share type definitions across XBee link.
    
    Solution: Define crate that houses common types.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Telemetry Types
+
+Helper crate that defines types shared between the stm32 app and the prometheus publisher.
---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use postcard::take_from_bytes;
+use serde::{Deserialize, Serialize};
+
+#[cfg(feature = "std")]
+use tokio_util::bytes::{Buf, BytesMut};
+#[cfg(feature = "std")]
+use tokio_util::codec::Decoder;
+
+pub use postcard;
+
+/// Root telemetry type.
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Eq)]
+pub struct Telemetry {
+    pub rpm: u32,
+}
+
+#[cfg(feature = "std")]
+pub struct PostCardCodec;
+
+#[cfg(feature = "std")]
+impl Decoder for PostCardCodec {
+    type Item = Telemetry;
+    type Error = std::io::Error;
+
+    /// Munch some bytes in order to build a T
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        match take_from_bytes::<Telemetry>(src) {
+            Ok((telemetry, remainder)) => {
+                src.advance(src.len() - remainder.len());
+                Ok(Some(telemetry))
+            }
+            Err(_) => Ok(None),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use postcard::to_slice;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(2, None)]
+    #[case(4, Some(Telemetry::default()))]
+    fn test_simple(#[case] index: usize, #[case] expect: Option<Telemetry>) {
+        let mut buffer = [0; 10];
+        // Serialize to buffer
+        to_slice(&Telemetry::default(), &mut buffer).unwrap();
+
+        // Deserialize through Decoder
+        let res = PostCardCodec
+            .decode(&mut BytesMut::from(&buffer[..index]))
+            .unwrap();
+
+        assert_eq!(res, expect);
+    }
+}
